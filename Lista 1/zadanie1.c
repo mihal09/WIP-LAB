013d5e5f346b961059983cba0b09f072ee9d3d10
mihal09 <mjanik1@op.pl>
2018-10-09 10:28:40 +0200

    zadanie 1

--- Lista 1/zadanie1.c
+++ Lista 1/zadanie1.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 int main()
 {
-char napis[] = "ABRAKADABRA";
-for(int i=0; i<strlen(napis); i++)
-    printf("%c ",napis[i]);
-return 0;
+    char napis[] = "ABRAKA";
+    int dlugosc = strlen(napis);
+    for(int i=dlugosc; i>0; i--) //i mowi ile znakow jest w danym wierszu
+    {   
+        for(int k=0; k<dlugosc-i; k++) //drukowanie spacji przed wierszem
+        {
+            printf(" ");
+        }
+        for(int j=0; j<i; j++)  //drukujemy tylko i znakow
+        {
+            printf("%c ",napis[j]);
+        }
+        printf("\n"); //koniec wiersza
+    }
+    return 0;
 }
 
