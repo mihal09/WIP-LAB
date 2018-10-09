d4880130edaed094f74f1eb3d28ed691ef71b7e2
mihal09 <mjanik1@op.pl>
2018-10-09 11:13:16 +0200

    zadanie 2

---
+++ Lista 1/zadanie2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+int main()
+{
+    float a,b,c;
+    scanf("%f %f %f", &a, &b, &c);
+    float delta = b*b - 4*a*c;
+    if(delta<0){
+        printf("W zbiorze liczb rzeczywistych nie ma rozwiązania tego równania/n");
+    }
+    else if(delta==0){
+        float x1 = -b/(2*a);
+        printf("%f/n",x1);
+    }
+    else {
+        float x1 = (-b-sqrt(delta))/(2*a);
+        float x2 = (-b+sqrt(delta))/(2*a);
+        printf("%f %f/n",x1, x2);
+    }
+    return 0;
+}
+
