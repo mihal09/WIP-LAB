d4727d9481f245f93ebae2e99e0992b8b35d14d4
mihal09 <mjanik1@op.pl>
2018-10-09 11:18:06 +0200

    typo

--- Lista 1/zadanie2.c
+++ Lista 1/zadanie2.c
@@ -7,16 +7,16 @@ int main()
     scanf("%f %f %f", &a, &b, &c);
     float delta = b*b - 4*a*c;
     if(delta<0){
-        printf("W zbiorze liczb rzeczywistych nie ma rozwiązania tego równania/n");
+        printf("W zbiorze liczb rzeczywistych nie ma rozwiązania tego równania\n");
     }
     else if(delta==0){
         float x1 = -b/(2*a);
-        printf("%f/n",x1);
+        printf("%f\n",x1);
     }
     else {
         float x1 = (-b-sqrt(delta))/(2*a);
         float x2 = (-b+sqrt(delta))/(2*a);
-        printf("%f %f/n",x1, x2);
+        printf("%f %f\n",x1, x2);
     }
     return 0;
 }
