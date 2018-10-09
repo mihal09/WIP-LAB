c7f4ee03dcb8746d2181eb2a25beab850011fcf5
mihal09 <mjanik1@op.pl>
2018-10-09 11:33:30 +0200

    zadanie 4

---
+++ Lista 1/zadanie4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%i", &n);
+    for(int i=0; i<n; i++) 
+    {
+        for(int k=0; k<n-i-1; k++) //gdy i rosnie, odstep ma malec
+            printf(" ");
+        for(int j=0; j<i*2+1; j++) //kazdy kolejny wiersz ma o 2 gwiazdki wiecej
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
